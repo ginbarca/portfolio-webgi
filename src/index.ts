40441139dfb36cbd9c9c1b67a0bacc149182c82f
Viet Anh Nguyen <nvanh@macbooks-MacBook-Pro.local>
2023-01-10 00:37:34 +0700

    Add default camera position

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,11 @@ import {
 	DataTextureLoader,
 	WebGLRenderer,
 	PointLight2,
-	SpotLight2
+	SpotLight2,
+	CameraUiPlugin,
+	PerspectiveCamera,
+	Vector3,
+	Vector2
 
 	// Color, // Import THREE.js internals
 	// Texture, // Import THREE.js internals
@@ -76,23 +80,49 @@ async function setupViewer() {
 	// await addBasePlugins(viewer)
 
 	// Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-	await viewer.addPlugin(CanvasSnipperPlugin)
 	await viewer.addPlugin(LightsUiPlugin)
+	await viewer.addPlugin(CameraUiPlugin)
 
+	// Add light
 	const scene = viewer.scene;
 	let spotLight = new SpotLight2(0xfefefe, 1, 0, 0.6424339472, 0.73);
 	spotLight.position.set(1.04, 1.34, 0.67);
 	spotLight.rotation.set(0, 0.2, 0);
 	// spotLight.castShadow = true;
-	let directionalRightLight = new DirectionalLight2(0x5293a4, 1);
+	let directionalRightLight = new DirectionalLight2(0xde07c5, 1);
 	directionalRightLight.rotation.set(0, 1.56, 0);
 	let directionalLeftLight = new DirectionalLight2(0x2c7256, 1);
 	directionalLeftLight.rotation.set(0, 4.46, 0);
 	await scene.addLight(spotLight);
 	await scene.addLight(directionalRightLight);
 	await scene.addLight(directionalLeftLight);
-	await scene.addLight(new SpotLight2());
+	let displayLight = new SpotLight2(0xfefefe, 1, 0, 0.3384126984, 0.47);
+	displayLight.position.set(-0.1, 2.12, 0.15);
+	displayLight.rotation.set(-1.64, 0, 0);
+	displayLight.castShadow = true;
+	await scene.addLight(displayLight);
 
+	// Config camera
+	// const cameraObj = new PerspectiveCamera();
+	// scene.activeCamera = viewer.createCamera(cameraObj);
+	// await scene.activeCamera.position.set(-0.1, 1.58, 2.38);
+	// await scene.activeCamera.positionUpdated();
+	// await scene.activeCamera.target.set(-0.18, 0.58, -0.10);
+	// await scene.activeCamera.targetUpdated();
+	const options = await scene.activeCamera.getCameraOptions();
+	options.zoom = 0.5;
+	options.position = [-0.1, 1.58, 2.38];
+	options.target = [-0.18, 0.58, -0.10];
+	viewer.scene.activeCamera.setCameraOptions(options);
+	const windowHalf = new Vector2( window.innerWidth / 2, window.innerHeight / 2 );
+	const mouse = new Vector2();
+
+	function onMouseMove(event: any) {
+		mouse.x = ( event.clientX - windowHalf.x );
+		mouse.y = ( event.clientY - windowHalf.y );
+	}
+	await document.addEventListener( 'mousemove', onMouseMove, false );
+	// await scene.activeCamera.positionTargetUpdated();
 
 	// This must be called once after all plugins are added.
 	viewer.renderer.refreshPipeline()
@@ -101,23 +131,11 @@ async function setupViewer() {
 
 	// Load an environment map if not set in the glb file
 	await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/evening_meadow_1k.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Chair_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Chair.Pied_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Desk.Drawers_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Desk.Handles_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Desk.Table_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Keyboard_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Lamp_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Moleskine_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Mouse_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Room_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Screen_baked.hdr', { generateMipmaps: false }));
-	// await scene.setEnvironment(await manager.importer!.importSinglePath<ITexture>('./assets/Screen.Frame_baked.hdr', { generateMipmaps: false }));
 
 	// Add some UI for tweak and testing.
 	const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
 	// Add plugins to the UI to see their settings.
-	uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin, LightsUiPlugin)
+	uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, LightsUiPlugin, CameraUiPlugin)
 
 }
 
